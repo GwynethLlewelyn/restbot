646bb560d28690e9cd58ae69ae9890084cf3bff3
gwynethllewelyn <gwynethllewelyn@1b3bde59-d7a3-319e-d2ac-4cc0b6bd8393>
2010-05-01 18:21:54 +0000

    group_UUID_activate changed to group_key_activate for consistency, and group_name_activate was added (to allow for setting group by name instead of by group UUID)

--- trunk/restbot-plugins/GroupsPlugin.cs
+++ trunk/restbot-plugins/GroupsPlugin.cs
@@ -31,18 +31,17 @@ using System.Threading;
 // group functions; based on TestClient.exe code
 namespace RESTBot
 {
-	public class ActivateGroupPlugin : StatefulPlugin
+	// Set active group, using group UUID
+	public class ActivateGroupKeyPlugin : StatefulPlugin
 	{
 		protected ManualResetEvent GroupsEvent = new ManualResetEvent(false);
-		// protected Dictionary<UUID, AutoResetEvent> ActivateGroupEvents = new Dictionary<UUID, AutoResetEvent>();
-		// protected Dictionary<UUID, String> groupThingy = new Dictionary<UUID, String>();
 				
 		private UUID session;
 		private string activeGroup;
 		
-		public ActivateGroupPlugin()
+		public ActivateGroupKeyPlugin()
 		{
-			MethodName = "group_UUID_activate";
+			MethodName = "group_key_activate";
 		}
 		
 		public override void Initialize(RestBot bot)
@@ -111,6 +110,129 @@ namespace RESTBot
                 GroupsEvent.Set();
             //}
         }	
-	// more commands to follow
 	}
+	
+	// Set active group, using group Name
+	public class ActivateGroupNamePlugin : StatefulPlugin
+	{
+		protected ManualResetEvent GroupsEvent = new ManualResetEvent(false);
+		public Dictionary<UUID, Group> GroupsCache = null;
+		private UUID session;
+		private string activeGroup;
+		
+		public ActivateGroupNamePlugin()
+		{
+			MethodName = "group_name_activate";
+		}
+		
+		public override void Initialize(RestBot bot)
+		{
+			session = bot.sessionid;
+			DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+		}
+		public override string Process(RestBot b, Dictionary<string, string> Parameters)
+		{
+			UUID groupUUID;
+			string groupName;
+			DebugUtilities.WriteDebug("TR - Entering group key parser");
+			try
+			{
+				if ( Parameters.ContainsKey("name") ) {
+					groupName = Parameters["name"].ToString().Replace("%20"," ").Replace("+"," ");
+				} else {
+					return "<error>arguments</error>";
+				}
+				DebugUtilities.WriteDebug("TR - Activating group");
+					
+				groupUUID = GroupName2UUID(b, groupName);
+            	if (UUID.Zero != groupUUID)
+            	{
+					string response = activateGroup(b, groupUUID);
+					DebugUtilities.WriteDebug("TR - Complete");
+					return "<active>" + response.Trim() + "</active>\n";
+				}
+				else
+				{
+					DebugUtilities.WriteDebug("TR - Error: group " + groupName + " doesn't exist");
+					return "<error>group name '" + groupName + "' doesn't exist.</error>";
+				}
+			}
+			catch ( Exception e )
+			{
+				DebugUtilities.WriteError(e.Message);
+				return "<error>parsekey</error>";
+			}
+			
+		}
+		
+        public void ReloadGroupsCache(RestBot b)
+        {
+            b.Client.Groups.CurrentGroups += Groups_CurrentGroups;            
+            b.Client.Groups.RequestCurrentGroups();
+            GroupsEvent.WaitOne(10000, false);
+            b.Client.Groups.CurrentGroups -= Groups_CurrentGroups;
+            GroupsEvent.Reset();
+        }
+
+        void Groups_CurrentGroups(object sender, CurrentGroupsEventArgs e)
+        {
+            if (null == GroupsCache)
+                GroupsCache = e.Groups;
+            else
+                lock (GroupsCache) { GroupsCache = e.Groups; }
+            GroupsEvent.Set();
+        }
+
+        public UUID GroupName2UUID(RestBot b, String groupName)
+        {
+            UUID tryUUID;
+            if (UUID.TryParse(groupName,out tryUUID))
+                    return tryUUID;
+            if (null == GroupsCache) {
+                    ReloadGroupsCache(b);
+                if (null == GroupsCache)
+                    return UUID.Zero;
+            }
+            lock(GroupsCache) {
+                if (GroupsCache.Count > 0) {
+                    foreach (Group currentGroup in GroupsCache.Values)
+                        if (currentGroup.Name.ToLower() == groupName.ToLower())
+                            return currentGroup.ID;
+                }
+            }
+            return UUID.Zero;
+        }
+		
+		private string activateGroup(RestBot b, UUID groupUUID)
+		{
+			DebugUtilities.WriteInfo(session.ToString() + " " + MethodName + " Activating group " + groupUUID.ToString());
+			EventHandler<PacketReceivedEventArgs> pcallback = AgentDataUpdateHandler;
+			b.Client.Network.RegisterCallback(PacketType.AgentDataUpdate, pcallback);
+			b.Client.Groups.ActivateGroup(groupUUID);
+			
+			if ( ! GroupsEvent.WaitOne(15000, true) ) {
+				DebugUtilities.WriteWarning(session + " " + MethodName + " timed out on setting active group");
+			}
+
+			// ignore everything and just reset the event
+			b.Client.Network.UnregisterCallback(PacketType.AgentDataUpdate, pcallback);
+			GroupsEvent.Reset();			
+
+			if (String.IsNullOrEmpty(activeGroup))
+			   	DebugUtilities.WriteWarning(session + " " + MethodName + " Failed to activate the group " + groupUUID);
+
+            return activeGroup;
+		}
+		
+		private void AgentDataUpdateHandler(object sender, PacketReceivedEventArgs e)
+        {
+            AgentDataUpdatePacket p = (AgentDataUpdatePacket)e.Packet;
+            //if (p.AgentData.AgentID == Client.Self.AgentID)
+            //{
+                activeGroup = Utils.BytesToString(p.AgentData.GroupName) + " (" + Utils.BytesToString(p.AgentData.GroupTitle) + ")";
+                GroupsEvent.Set();
+            //}
+        }	
+	}
+
 }
