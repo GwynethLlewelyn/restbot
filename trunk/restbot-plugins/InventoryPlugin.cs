d95edd6cbd7cf2a656984a35e7f4084f49677596
gwynethllewelyn <gwynethllewelyn@1b3bde59-d7a3-319e-d2ac-4cc0b6bd8393>
2010-06-05 14:06:43 +0000

    Added some information on LINKS and README (compilation instructions); attempted to get the Windows compilation build files corrected; added the new commands, list_item and give_item, to InventoryPlugin

--- trunk/restbot-plugins/InventoryPlugin.cs
+++ trunk/restbot-plugins/InventoryPlugin.cs
@@ -25,13 +25,16 @@ using System.Collections.Generic;
 using System.Text;
 using OpenMetaverse;
 using OpenMetaverse.Packets;
+using System.Xml;
+using System.Xml.Serialization;
 using System.Net;
+using System.IO;
 using System.Threading;
 
-// group functions; based on TestClient.exe code
+// inventory functions; based on TestClient.exe code
 namespace RESTBot
 {
-	// Set active group, using group UUID
+	// recursively list inventory, starting from folder ID. If no folder ID given, start from root
 	public class ListInventoryPlugin : StatefulPlugin
 	{
 		private UUID session;
@@ -121,4 +124,192 @@ namespace RESTBot
 		}
 
 	}
+	
+	// List inventory item; params are (inventory) item ID
+	public class ListItemPlugin : StatefulPlugin
+	{
+		private UUID session;
+		
+		public ListItemPlugin()
+		{
+			MethodName = "list_item";
+		}
+		
+		public override void Initialize(RestBot bot)
+		{
+			session = bot.sessionid;
+			DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+		}
+		
+		public override string Process(RestBot b, Dictionary<string, string> Parameters)
+		{
+			UUID itemID;
+			DebugUtilities.WriteDebug("List Item - Entering key parser");
+			try
+			{
+				bool check = false;
+				if (Parameters.ContainsKey("key"))
+				{
+					DebugUtilities.WriteDebug("LI - Attempting to parse from POST");
+					check = UUID.TryParse(Parameters["key"].ToString().Replace("_"," "), out itemID);
+					DebugUtilities.WriteDebug("LI - Succesfully parsed POST");
+				}
+				else
+				{
+					return "<error>parsekey</error>";
+				}
+
+				if (check)	// means that we have a correctly parsed key	
+				{
+					DebugUtilities.WriteDebug("List Item - Fetching " + itemID.ToString());
+						
+					InventoryItem oneItem;
+						
+					oneItem = b.Client.Inventory.FetchItem(itemID, b.Client.Self.AgentID, 5000);
+					// to-do: catch timeout explicitly
+					
+					if (oneItem == null)
+					{
+						return "<error>item " + itemID.ToString() + " not found</error>\n";
+					}
+					
+					string response;
+					
+					response  = "<AssetUUID>" + oneItem.AssetUUID.ToString() + "</AssetUUID>";
+					response += "<PermissionsOwner>" + PermMaskString(oneItem.Permissions.OwnerMask) + "</PermissionsOwner>";
+					response += "<PermissionsGroup>" + PermMaskString(oneItem.Permissions.GroupMask) + "</PermissionsGroup>";					response += "<AssetType>" + oneItem.AssetType.ToString() + "</AssetType>";
+					response += "<InventoryType>" + oneItem.InventoryType.ToString() + "</InventoryType>";
+					response += "<CreatorID>" + oneItem.CreatorID.ToString() + "</CreatorID>";
+					response += "<Description>" + oneItem.Description.ToString() + "</Description>";
+					response += "<GroupID>" + oneItem.GroupID.ToString() + "</GroupID>";
+					response += "<GroupOwned>" + oneItem.GroupOwned.ToString() + "</GroupOwned>";
+					response += "<SalePrice>" + oneItem.SalePrice.ToString() + "</SalePrice>";
+					response += "<SaleType>" + oneItem.SaleType.ToString() + "</SaleType>";
+					response += "<Flags>" + oneItem.Flags.ToString() + "</Flags>";
+					response += "<CreationDate>" + oneItem.CreationDate.ToString() + "</CreationDate>";
+					response += "<LastOwnerID>" + oneItem.LastOwnerID.ToString() + "</LastOwnerID>";
+						
+					return "<item>" + response + "</item>\n";
+				}
+				else 
+				{
+					return "<error>parsekey</error>";
+				}
+			}
+			catch ( Exception e )
+			{
+				DebugUtilities.WriteError(e.Message);
+				return "<error>" + e.Message + "</error>";
+			}
+		}
+
+		/// <summary>
+		/// Returns a 3-character summary of the PermissionMask
+		/// CMT if the mask allows copy, mod and transfer
+		/// -MT if it disallows copy
+		/// --T if it only allows transfer
+		/// --- if it disallows everything
+		/// </summary>
+		/// <param name="mask"></param>
+		/// <returns></returns>
+		private static string PermMaskString(PermissionMask mask)
+		{
+			string str = "";
+			if (((uint)mask | (uint)PermissionMask.Copy) == (uint)PermissionMask.Copy)
+				str += "C";
+			else
+				str += "-";
+			if (((uint)mask | (uint)PermissionMask.Modify) == (uint)PermissionMask.Modify)
+				str += "M";
+			else
+				str += "-";
+			if (((uint)mask | (uint)PermissionMask.Transfer) == (uint)PermissionMask.Transfer)
+				str += "T";
+			else
+				str += "-";
+			return str;
+		}
+	} // end give_inventory
+	
+		// Give inventory item; params are (inventory) itemID and avatarKey
+	public class GiveItemPlugin : StatefulPlugin
+	{
+		private UUID session;
+		
+		public GiveItemPlugin()
+		{
+			MethodName = "give_item";
+		}
+		
+		public override void Initialize(RestBot bot)
+		{
+			session = bot.sessionid;
+			DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+		}
+		
+		public override string Process(RestBot b, Dictionary<string, string> Parameters)
+		{
+			UUID itemID, avatarKey;
+			InventoryManager Manager;
+        
+			DebugUtilities.WriteDebug("Give Item - Entering key parser");
+			try
+			{
+				bool check = false;
+				if (Parameters.ContainsKey("itemID"))
+				{
+					DebugUtilities.WriteDebug("GI - Attempting to parse from POST");
+					check = UUID.TryParse(Parameters["itemID"].ToString().Replace("_"," "), out itemID);
+					
+					
+					if (check)
+					{
+						check = UUID.TryParse(Parameters["avatarKey"].ToString().Replace("_"," "), out avatarKey);
+						DebugUtilities.WriteDebug("GI - Succesfully parsed POST");
+					}
+					else
+					{
+						return "<error>parsekey itemID</error>";
+					}
+				}
+				else
+				{
+					return "<error>parsekey</error>";
+				}
+
+				if (check)	// means that we have a correctly parsed key	
+				{
+					DebugUtilities.WriteDebug("Give Item " + itemID.ToString() + " to avatar" + avatarKey.ToString());
+					
+					// Extract item information from inventory
+					InventoryItem oneItem;
+						
+					oneItem = b.Client.Inventory.FetchItem(itemID, b.Client.Self.AgentID, 5000);
+					// to-do: catch timeout explicitly
+					
+					if (oneItem == null)
+					{
+						return "<error>item " + itemID.ToString() + " not found</error>\n";
+					}
+					
+					// attempt to send it to the avatar
+					
+					Manager = b.Client.Inventory;
+            							
+					Manager.GiveItem(oneItem.UUID, oneItem.Name, oneItem.AssetType, avatarKey, false);
+						
+					return "<item><name>" + oneItem.Name + "</name><assetType>" + oneItem.AssetType + "</assetType><itemID>" + itemID.ToString() + "</itemID><avatarKey>" + avatarKey.ToString() + "</avatarKey></item>\n";
+				}
+				else 
+				{
+					return "<error>parsekey avatarKey</error>";
+				}
+			}
+			catch ( Exception e )
+			{
+				DebugUtilities.WriteError(e.Message);
+				return "<error>" + e.Message + "</error>";
+			}
+		}
+	} // end give item
 }
