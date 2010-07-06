9673fc4de45e9bc83834fad83ab4a932185823a3
gwynethllewelyn <gwynethllewelyn@1b3bde59-d7a3-319e-d2ac-4cc0b6bd8393>
2010-07-06 17:38:25 +0000

    Added notecard creation, with (one) attachment inside notecard

--- trunk/restbot-plugins/InventoryPlugin.cs
+++ trunk/restbot-plugins/InventoryPlugin.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using OpenMetaverse;
+using OpenMetaverse.Assets;
 using OpenMetaverse.Packets;
 using System.Xml;
 using System.Xml.Serialization;
@@ -231,74 +232,72 @@ namespace RESTBot
 		}
 	} // end give_inventory
 	
-		// Give inventory item; params are (inventory) itemID and avatarKey
 	public class GiveItemPlugin : StatefulPlugin
 	{
-		private UUID session;
+	private UUID session;
 		
-		public GiveItemPlugin()
-		{
-			MethodName = "give_item";
-		}
+	public GiveItemPlugin()
+	{
+		MethodName = "give_item";
+	}
 		
-		public override void Initialize(RestBot bot)
-		{
-			session = bot.sessionid;
-			DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
-		}
+	public override void Initialize(RestBot bot)
+	{
+		session = bot.sessionid;
+		DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+	}
 		
-		public override string Process(RestBot b, Dictionary<string, string> Parameters)
+	public override string Process(RestBot b, Dictionary<string, string> Parameters)
+	{
+		UUID itemID, avatarKey;
+		InventoryManager Manager;
+		
+		DebugUtilities.WriteDebug("Give Item - Entering key parser");
+		try
 		{
-			UUID itemID, avatarKey;
-			InventoryManager Manager;
-        
-			DebugUtilities.WriteDebug("Give Item - Entering key parser");
-			try
+			bool check = false;
+			if (Parameters.ContainsKey("itemID"))
 			{
-				bool check = false;
-				if (Parameters.ContainsKey("itemID"))
+				DebugUtilities.WriteDebug("GI - Attempting to parse from POST");
+				check = UUID.TryParse(Parameters["itemID"].ToString().Replace("_"," "), out itemID);				
+					
+				if (check)
 				{
-					DebugUtilities.WriteDebug("GI - Attempting to parse from POST");
-					check = UUID.TryParse(Parameters["itemID"].ToString().Replace("_"," "), out itemID);
-					
-					
-					if (check)
-					{
-						check = UUID.TryParse(Parameters["avatarKey"].ToString().Replace("_"," "), out avatarKey);
-						DebugUtilities.WriteDebug("GI - Succesfully parsed POST");
-					}
-					else
-					{
-						return "<error>parsekey itemID</error>";
-					}
+					check = UUID.TryParse(Parameters["avatarKey"].ToString().Replace("_"," "), out avatarKey);
+					DebugUtilities.WriteDebug("GI - Succesfully parsed POST");
 				}
 				else
 				{
-					return "<error>parsekey</error>";
+					return "<error>parsekey itemID</error>";
 				}
+			}
+			else
+			{
+				return "<error>parsekey</error>";
+			}
 
-				if (check)	// means that we have a correctly parsed key	
+			if (check)	// means that we have a correctly parsed key	
+			{
+				DebugUtilities.WriteDebug("Give Item " + itemID.ToString() + " to avatar" + avatarKey.ToString());
+					
+				// Extract item information from inventory
+				InventoryItem oneItem;
+					
+				oneItem = b.Client.Inventory.FetchItem(itemID, b.Client.Self.AgentID, 5000);
+				// to-do: catch timeout explicitly
+					
+				if (oneItem == null)
 				{
-					DebugUtilities.WriteDebug("Give Item " + itemID.ToString() + " to avatar" + avatarKey.ToString());
+					return "<error>item " + itemID.ToString() + " not found</error>\n";
+				}
 					
-					// Extract item information from inventory
-					InventoryItem oneItem;
+				// attempt to send it to the avatar
+					
+				Manager = b.Client.Inventory;
+										
+				Manager.GiveItem(oneItem.UUID, oneItem.Name, oneItem.AssetType, avatarKey, false);
 						
-					oneItem = b.Client.Inventory.FetchItem(itemID, b.Client.Self.AgentID, 5000);
-					// to-do: catch timeout explicitly
-					
-					if (oneItem == null)
-					{
-						return "<error>item " + itemID.ToString() + " not found</error>\n";
-					}
-					
-					// attempt to send it to the avatar
-					
-					Manager = b.Client.Inventory;
-            							
-					Manager.GiveItem(oneItem.UUID, oneItem.Name, oneItem.AssetType, avatarKey, false);
-						
-					return "<item><name>" + oneItem.Name + "</name><assetType>" + oneItem.AssetType + "</assetType><itemID>" + itemID.ToString() + "</itemID><avatarKey>" + avatarKey.ToString() + "</avatarKey></item>\n";
+				return "<item><name>" + oneItem.Name + "</name><assetType>" + oneItem.AssetType + "</assetType><itemID>" + itemID.ToString() + "</itemID><avatarKey>" + avatarKey.ToString() + "</avatarKey></item>\n";
 				}
 				else 
 				{
@@ -312,4 +311,228 @@ namespace RESTBot
 			}
 		}
 	} // end give item
+
+	// Create Notecard; params are notecard name, notecard (textual data) and optionally a key (item UUID) to embed an item inside the notecard
+	public class CreateNotecardPlugin : StatefulPlugin
+	{
+		private UUID session;
+        const int NOTECARD_CREATE_TIMEOUT = 1000 * 10;
+        const int NOTECARD_FETCH_TIMEOUT = 1000 * 10;
+        const int INVENTORY_FETCH_TIMEOUT = 1000 * 10;		
+		
+		public CreateNotecardPlugin()
+		{
+			MethodName = "create_notecard";
+		}
+		
+		public override void Initialize(RestBot bot)
+		{
+			session = bot.sessionid;
+			DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+		}
+		
+		public override string Process(RestBot b, Dictionary<string, string> Parameters)
+		{
+			UUID embedItemID = UUID.Zero;
+			string notecardName, notecardData;
+		
+			DebugUtilities.WriteDebug("Create Notecard - Entering key parser");
+			try
+			{		
+				// item ID to embed is optional; handle later
+				if (Parameters.ContainsKey("key"))
+				{
+					UUID.TryParse(Parameters["key"].ToString().Replace("_"," "), out embedItemID);
+				}			
+				
+				// notecard data is required!				
+				if (Parameters.ContainsKey("notecard"))
+				{
+					DebugUtilities.WriteDebug("CN - Attempting to parse notecard data from POST");
+					notecardData = Parameters["notecard"];
+					
+									}
+				else
+				{
+					return "<error>notecard text data not found</error>";
+				}
+				
+				// notecard name is optional, we'll assign a random name
+				
+				if (Parameters.ContainsKey("name"))
+				{
+					DebugUtilities.WriteDebug("CN - Attempting to parse notecard name from POST");
+					notecardName = Parameters["name"];
+
+					DebugUtilities.WriteDebug("CN - Succesfully parsed POST");
+				}
+				else
+				{
+					notecardName = "(no name)";
+				}
+				
+				
+				UUID notecardItemID = UUID.Zero, notecardAssetID = UUID.Zero;
+				bool success = false, finalUploadSuccess = false;
+				string message = String.Empty;
+				AutoResetEvent notecardEvent = new AutoResetEvent(false);
+				
+
+				DebugUtilities.WriteDebug("Notecard data ('" + notecardName + "') found: '" + notecardData + "'");
+					
+				 #region Notecard asset data
+	
+				 AssetNotecard notecard = new AssetNotecard();
+				 notecard.BodyText = notecardData;
+	
+				 // Item embedding
+				 if (embedItemID != UUID.Zero)
+				 {
+					 // Try to fetch the inventory item
+					 InventoryItem item = FetchItem(b, embedItemID);
+					 if (item != null)
+					 {
+						 notecard.EmbeddedItems = new List<InventoryItem> { item };
+						 notecard.BodyText += (char)0xdbc0 + (char)0xdc00;
+					 }
+					 else
+					 {
+						 return "Failed to fetch inventory item " + embedItemID;
+					 }
+				 }
+	
+				 notecard.Encode();
+	
+				 #endregion Notecard asset data
+				b.Client.Inventory.RequestCreateItem(b.Client.Inventory.FindFolderForType(AssetType.Notecard),
+				notecardName, notecardName + " created by OpenMetaverse RESTbot " + DateTime.Now, AssetType.Notecard,
+				UUID.Random(), InventoryType.Notecard, PermissionMask.All,
+				delegate(bool createSuccess, InventoryItem item)
+				{
+					if (createSuccess)
+					{
+						#region Upload an empty notecard asset first
+
+						AutoResetEvent emptyNoteEvent = new AutoResetEvent(false);
+						AssetNotecard empty = new AssetNotecard();
+						empty.BodyText = "\n";
+						empty.Encode();
+
+						b.Client.Inventory.RequestUploadNotecardAsset(empty.AssetData, item.UUID,
+							delegate(bool uploadSuccess, string status, UUID itemID, UUID assetID)
+							{
+								notecardItemID = itemID;
+								notecardAssetID = assetID;
+								success = uploadSuccess;
+								message = status ?? "Unknown error uploading notecard asset";
+								emptyNoteEvent.Set();
+							});
+
+						emptyNoteEvent.WaitOne(NOTECARD_CREATE_TIMEOUT, false);
+
+						#endregion Upload an empty notecard asset first
+
+						if (success)
+						{
+							// Upload the actual notecard asset
+							b.Client.Inventory.RequestUploadNotecardAsset(notecard.AssetData, item.UUID,
+								delegate(bool uploadSuccess, string status, UUID itemID, UUID assetID)
+								{
+									notecardItemID = itemID;
+									notecardAssetID = assetID;
+									finalUploadSuccess = uploadSuccess;
+									message = status ?? "Unknown error uploading notecard asset";
+									notecardEvent.Set();
+								});
+						}
+						else
+						{
+							notecardEvent.Set();
+						}
+					}
+					else
+					{
+						message = "Notecard item creation failed";
+						notecardEvent.Set();
+					}
+				}
+			);
+
+			notecardEvent.WaitOne(NOTECARD_CREATE_TIMEOUT, false);
+
+			// DebugUtilities.WriteDebug("Notecard possibly created, ItemID " + notecardItemID + " AssetID " + notecardAssetID + " Content: '" + DownloadNotecard(b, notecardItemID, notecardAssetID) + "'");
+
+			if (finalUploadSuccess)
+			{
+				DebugUtilities.WriteDebug("Notecard successfully created, ItemID " + notecardItemID + " AssetID " + notecardAssetID + " Content: '" + 				DownloadNotecard(b, notecardItemID, notecardAssetID) + "'");
+				return "<notecard><ItemID>" + notecardItemID + "</ItemID><AssetID>" + notecardAssetID + "</AssetID><name>" + notecardName + "</name></notecard>";
+			}
+			else
+				return "<error>Notecard creation failed: " + message + "</error>";
+
+			}
+			catch ( Exception e )
+			{
+				DebugUtilities.WriteError(e.Message);
+				return "<error>" + e.Message + "</error>";
+			}
+		}
+		
+		// Accessory functions
+		
+		private InventoryItem FetchItem(RestBot b, UUID itemID)
+		{
+			InventoryItem fetchItem = null;
+			AutoResetEvent fetchItemEvent = new AutoResetEvent(false);
+
+			EventHandler<ItemReceivedEventArgs> itemReceivedCallback =
+				delegate(object sender, ItemReceivedEventArgs e)
+				{
+					if (e.Item.UUID == itemID)
+					{
+						fetchItem = e.Item;
+						fetchItemEvent.Set();
+					}
+				};
+
+			b.Client.Inventory.ItemReceived += itemReceivedCallback;
+
+			b.Client.Inventory.RequestFetchInventory(itemID, b.Client.Self.AgentID);
+
+			fetchItemEvent.WaitOne(INVENTORY_FETCH_TIMEOUT, false);
+
+			b.Client.Inventory.ItemReceived -= itemReceivedCallback;
+
+			return fetchItem;
+		}
+
+		private string DownloadNotecard(RestBot b, UUID itemID, UUID assetID)
+		{
+			AutoResetEvent assetDownloadEvent = new AutoResetEvent(false);
+			byte[] notecardData = null;
+			string error = "Timeout";
+
+			b.Client.Assets.RequestInventoryAsset(assetID, itemID, UUID.Zero, b.Client.Self.AgentID, AssetType.Notecard, true,
+								delegate(AssetDownload transfer, Asset asset)
+								{
+									if (transfer.Success)
+										notecardData = transfer.AssetData;
+									else
+										error = transfer.Status.ToString();
+									assetDownloadEvent.Set();
+								}
+			);
+
+			assetDownloadEvent.WaitOne(NOTECARD_FETCH_TIMEOUT, false);
+
+			if (notecardData != null)
+				return Encoding.UTF8.GetString(notecardData);
+			else
+				return "Error downloading notecard asset: " + error;
+		}
+		
+		
+	} // end create notecard
+
+
 }
