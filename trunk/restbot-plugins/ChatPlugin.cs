f5e2c2b3ecd6c6ff0d5a46a8c6bef7f4127ad40c
bkrisler <bkrisler@1b3bde59-d7a3-319e-d2ac-4cc0b6bd8393>
2011-05-17 17:01:12 +0000

    New plugin for chat messages. Maybe this might get rolled into a more generic plugin, but for now it is fine here.

---
+++ trunk/restbot-plugins/ChatPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenMetaverse;
+
+namespace RESTBot.restbot_plugins
+{
+    public class SayPlugin : StatefulPlugin
+    {
+        private UUID session;
+        private RestBot me;
+
+        public SayPlugin()
+        {
+            MethodName = "say";
+        }
+
+        public override void Initialize(RestBot bot)
+        {
+            session = bot.sessionid;
+            me = bot;
+            DebugUtilities.WriteDebug(session + " " + MethodName + " startup");
+
+            base.Initialize(bot);
+        }
+
+        public override string Process(RestBot b, Dictionary<string, string> Paramaters)
+        {
+            int channel = 0;        
+            bool check = true;
+            string message = String.Empty;
+
+            if (Paramaters.ContainsKey("channel"))
+            {
+                check &= int.TryParse(Paramaters["channel"], out channel);
+            }
+
+            if (Paramaters.ContainsKey("message"))
+            {
+                message = Paramaters["message"].ToString().Replace("+", " ");
+            }
+            else check = false;
+
+            // Make sure we are not in autopilot.
+            b.Client.Self.AutoPilotCancel();
+
+            b.Client.Self.Chat(message, channel, ChatType.Normal);
+
+            return "<say><channel>" +channel+"</channel><message>"+ message.ToString() + "</message></say>";
+        }
+    }
+}
