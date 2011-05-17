58ed501287eae4e00e96f2fbab9d13c1b99cd65f
bkrisler <bkrisler@1b3bde59-d7a3-319e-d2ac-4cc0b6bd8393>
2011-05-17 17:05:25 +0000

    Added header

--- restbot-plugins/PrimsPlugin.cs
+++ restbot-plugins/PrimsPlugin.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/*--------------------------------------------------------------------------------
+ LICENSE:
+         This file is part of the RESTBot Project.
+ 
+         RESTbot is free software; you can redistribute it and/or modify it under
+         the terms of the Affero General Public License Version 1 (March 2002)
+ 
+         RESTBot is distributed in the hope that it will be useful,
+         but WITHOUT ANY WARRANTY; without even the implied warranty of
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+         Affero General Public License for more details.
+
+         You should have received a copy of the Affero General Public License
+         along with this program (see ./LICENSING) If this is missing, please 
+         contact alpha.zaius[at]gmail[dot]com and refer to 
+         <http://www.gnu.org/licenses/agpl.html> for now.
+         
+         Author: Brian Krisler bkrisler@gmail.com
+
+ COPYRIGHT: 
+         RESTBot Codebase (c) 2010-2011 Raytheon BBN Technologies
+--------------------------------------------------------------------------------*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 using OpenMetaverse;
