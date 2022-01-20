9d818dca4e1b749afeb1b2e7522c18c93062f131
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2022-01-20 18:00:11 +0000

    Prettification and comments

--- restbot-plugins/StatsPlugin.cs
+++ restbot-plugins/StatsPlugin.cs
@@ -11,11 +11,11 @@
 
 		This program is distributed in the hope that it will be useful,
 		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
 		GNU Affero General Public License for more details.
 
 		You should have received a copy of the GNU Affero General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		along with this program.	If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
@@ -24,74 +24,96 @@ using OpenMetaverse;
 using System.Net;
 using System.Threading;
 
-// avatar info functions
 namespace RESTBot
 {
-    public class DilationPlugin : RestPlugin
-    {
-        public DilationPlugin()
-        {
-            MethodName = "dilation";
-        }
-
-        public override string Process(RestBot b, Dictionary<string, string> Paramaters)
-        {
-			b.Client.Settings.ALWAYS_DECODE_OBJECTS=true;
-
-            string dilation = b.Client.Network.CurrentSim.Stats.Dilation.ToString();
-            b.Client.Settings.ALWAYS_DECODE_OBJECTS = false;
-			return("<dilation>" + dilation + "</dilation>\n");
-		}
-    }
-
-    public class SimStatPlugin : RestPlugin
-    {
-        public SimStatPlugin()
-        {
-            MethodName = "sim_stat";
-        }
-
-        public override string Process(RestBot b, Dictionary<string, string> Paramaters)
-        {
-			bool check = false;
-			while ( !check ) {
-				if ( b.Client.Network.CurrentSim.Stats.FPS != 0 ) {
-					check = true;
+		/// <summary>
+		/// Class to return the current simulator dilation (a measure of lag).
+		/// </summary>
+		public class DilationPlugin : RestPlugin
+		{
+				/// <summary>
+				/// Sets the plugin name for the router.
+				/// </summary>
+				public DilationPlugin()
+				{
+						MethodName = "dilation";
 				}
-			}
-			string response = "<stats>\n";
-			response += "<dilation>" + b.Client.Network.CurrentSim.Stats.Dilation.ToString() + "</dilation>\n";
-			response += "<inbps>" + b.Client.Network.CurrentSim.Stats.IncomingBPS.ToString() + "</inbps>\n";
-			response += "<outbps>" + b.Client.Network.CurrentSim.Stats.OutgoingBPS.ToString() + "</outbps>\n";
-			response += "<resentout>" + b.Client.Network.CurrentSim.Stats.ResentPackets.ToString() + "</resentout>\n";
-			response += "<resentin>" + b.Client.Network.CurrentSim.Stats.ReceivedResends.ToString() + "</resentin>\n";
-			response += "<queue>" + b.Client.Network.InboxCount.ToString() + "</queue>\n";
-			response += "<fps>" + b.Client.Network.CurrentSim.Stats.FPS.ToString() + "</fps>\n";
-			response += "<physfps>" + b.Client.Network.CurrentSim.Stats.PhysicsFPS.ToString() + "</physfps>\n";
-			response += "<agentupdates>" + b.Client.Network.CurrentSim.Stats.AgentUpdates.ToString() + "</agentupdates>\n";
-			response += "<objects>" + b.Client.Network.CurrentSim.Stats.Objects.ToString() + "</objects>\n";
-			response += "<scriptedobjects>" + b.Client.Network.CurrentSim.Stats.ScriptedObjects.ToString() + "</scriptedobjects>\n";
-			response += "<agents>" + b.Client.Network.CurrentSim.Stats.Agents.ToString() + "</agents>\n";
-			response += "<childagents>" + b.Client.Network.CurrentSim.Stats.ChildAgents.ToString() + "</childagents>\n";
-			response += "<activescripts>" + b.Client.Network.CurrentSim.Stats.ActiveScripts.ToString() + "</activescripts>\n";
-			response += "<lslips>" + b.Client.Network.CurrentSim.Stats.LSLIPS.ToString() + "</lslips>\n";
-			response += "<inpps>" + b.Client.Network.CurrentSim.Stats.INPPS.ToString() + "</inpps>\n";
-			response += "<outpps>" + b.Client.Network.CurrentSim.Stats.OUTPPS.ToString() + "</outpps>\n";
-			response += "<pendingdownloads>" + b.Client.Network.CurrentSim.Stats.PendingDownloads.ToString() + "</pendingdownloads>\n";
-			response += "<pendinguploads>" + b.Client.Network.CurrentSim.Stats.PendingUploads.ToString() + "</pendinguploads>\n";
-			response += "<virtualsize>" + b.Client.Network.CurrentSim.Stats.VirtualSize.ToString() + "</virtualsize>\n";
-			response += "<residentsize>" + b.Client.Network.CurrentSim.Stats.ResidentSize.ToString() + "</residentsize>\n";
-			response += "<pendinglocaluploads>" + b.Client.Network.CurrentSim.Stats.PendingLocalUploads.ToString() + "</pendinglocaluploads>\n";
-			response += "<unackedbytes>" + b.Client.Network.CurrentSim.Stats.UnackedBytes.ToString() + "</unackedbytes>\n";
-			response += "<time>\n";
-			response += "<frame>" + b.Client.Network.CurrentSim.Stats.FrameTime.ToString() + "</frame>\n";
-			response += "<image>" + b.Client.Network.CurrentSim.Stats.ImageTime.ToString() + "</image>\n";
-			response += "<physics>" + b.Client.Network.CurrentSim.Stats.PhysicsTime.ToString() + "</physics>\n";
-			response += "<script>" + b.Client.Network.CurrentSim.Stats.ScriptTime.ToString() + "</script>\n";
-			response += "<other>" + b.Client.Network.CurrentSim.Stats.OtherTime.ToString() + "</other>\n";
-			response += "</time>\n";
-			response += "</stats>\n";
-            return (response);
+
+				/// <summary>
+				/// Handler event for this plugin.
+				/// </summary>
+				/// <param name="b">A currently active RestBot</param>
+				/// <param name="Parameters">not used</param>
+				public override string Process(RestBot b, Dictionary<string, string> Parameters)
+				{
+						b.Client.Settings.ALWAYS_DECODE_OBJECTS = true;
+
+						string dilation = b.Client.Network.CurrentSim.Stats.Dilation.ToString();
+						b.Client.Settings.ALWAYS_DECODE_OBJECTS = false;
+						return("<dilation>" + dilation + "</dilation>\n");
+				}
 		}
-    }
+
+		/// <summary>
+		/// Class to return full statistics of the simulator the 'bot is in.
+		/// </summary>
+		public class SimStatPlugin : RestPlugin
+		{
+				/// <summary>
+				/// Sets the plugin name for the router.
+				/// </summary>
+				public SimStatPlugin()
+				{
+						MethodName = "sim_stat";
+				}
+
+				/// <summary>
+				/// Handler event for this plugin.
+				/// </summary>
+				/// <param name="b">A currently active RestBot</param>
+				/// <param name="Parameters">not used</param>
+				/// <remark>This will list statistics only for the simulator where the 'bot currently is.</remark>
+				public override string Process(RestBot b, Dictionary<string, string> Parameters)
+				{
+						bool check = false;
+						while ( !check ) {
+								if ( b.Client.Network.CurrentSim.Stats.FPS != 0 ) {
+								check = true;
+						}
+				}
+				string response = "<stats>\n";
+				response += "\t<dilation>" + b.Client.Network.CurrentSim.Stats.Dilation.ToString() + "</dilation>\n";
+				response += "\t<inbps>" + b.Client.Network.CurrentSim.Stats.IncomingBPS.ToString() + "</inbps>\n";
+				response += "\t<outbps>" + b.Client.Network.CurrentSim.Stats.OutgoingBPS.ToString() + "</outbps>\n";
+				response += "\t<resentout>" + b.Client.Network.CurrentSim.Stats.ResentPackets.ToString() + "</resentout>\n";
+				response += "\t<resentin>" + b.Client.Network.CurrentSim.Stats.ReceivedResends.ToString() + "</resentin>\n";
+				response += "\t<queue>" + b.Client.Network.InboxCount.ToString() + "</queue>\n";
+				response += "\t<fps>" + b.Client.Network.CurrentSim.Stats.FPS.ToString() + "</fps>\n";
+				response += "\t<physfps>" + b.Client.Network.CurrentSim.Stats.PhysicsFPS.ToString() + "</physfps>\n";
+				response += "\t<agentupdates>" + b.Client.Network.CurrentSim.Stats.AgentUpdates.ToString() + "</agentupdates>\n";
+				response += "\t<objects>" + b.Client.Network.CurrentSim.Stats.Objects.ToString() + "</objects>\n";
+				response += "\t<scriptedobjects>" + b.Client.Network.CurrentSim.Stats.ScriptedObjects.ToString() + "</scriptedobjects>\n";
+				response += "\t<agents>" + b.Client.Network.CurrentSim.Stats.Agents.ToString() + "</agents>\n";
+				response += "\t<childagents>" + b.Client.Network.CurrentSim.Stats.ChildAgents.ToString() + "</childagents>\n";
+				response += "\t<activescripts>" + b.Client.Network.CurrentSim.Stats.ActiveScripts.ToString() + "</activescripts>\n";
+				response += "\t<lslips>" + b.Client.Network.CurrentSim.Stats.LSLIPS.ToString() + "</lslips>\n";
+				response += "\t<inpps>" + b.Client.Network.CurrentSim.Stats.INPPS.ToString() + "</inpps>\n";
+				response += "\t<outpps>" + b.Client.Network.CurrentSim.Stats.OUTPPS.ToString() + "</outpps>\n";
+				response += "\t<pendingdownloads>" + b.Client.Network.CurrentSim.Stats.PendingDownloads.ToString() + "</pendingdownloads>\n";
+				response += "\t<pendinguploads>" + b.Client.Network.CurrentSim.Stats.PendingUploads.ToString() + "</pendinguploads>\n";
+				response += "\t<virtualsize>" + b.Client.Network.CurrentSim.Stats.VirtualSize.ToString() + "</virtualsize>\n";
+				response += "\t<residentsize>" + b.Client.Network.CurrentSim.Stats.ResidentSize.ToString() + "</residentsize>\n";
+				response += "\t<pendinglocaluploads>" + b.Client.Network.CurrentSim.Stats.PendingLocalUploads.ToString() + "</pendinglocaluploads>\n";
+				response += "\t<unackedbytes>" + b.Client.Network.CurrentSim.Stats.UnackedBytes.ToString() + "</unackedbytes>\n";
+				response += "\t<time>\n";
+				response += "\t\t<frame>" + b.Client.Network.CurrentSim.Stats.FrameTime.ToString() + "</frame>\n";
+				response += "\t\t<image>" + b.Client.Network.CurrentSim.Stats.ImageTime.ToString() + "</image>\n";
+				response += "\t\t<physics>" + b.Client.Network.CurrentSim.Stats.PhysicsTime.ToString() + "</physics>\n";
+				response += "\t\t<script>" + b.Client.Network.CurrentSim.Stats.ScriptTime.ToString() + "</script>\n";
+				response += "\t\t<other>" + b.Client.Network.CurrentSim.Stats.OtherTime.ToString() + "</other>\n";
+				response += "\t\t</time>\n";
+				response += "\t\t</stats>\n";
+				return (response);
+				}
+		}
 }
