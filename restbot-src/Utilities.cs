81e85c4c195e88cdfa861b9a9f30e49be93d62a8
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2022-01-26 17:00:57 +0000

    Refactor: moving getName/getKey to a new Utilities class
    name2key/key2name are constantly being called for all sorts of purposes, so the idea is to move them to a special static utility class, and then use it instead (that way, the cache will be common for all classes, instances, and methods)

---
+++ restbot-src/Utilities.cs
@@ -0,0 +1,182 @@
+/*--------------------------------------------------------------------------------
+	FILE INFORMATION:
+		Name: Utilities.cs [./restbot-src/Utilities.cs]
+		Description: Namespace to handle all sorts of utility functions which do not
+								 fit anywhere else (gwyneth 20220126).
+
+	LICENSE:
+		This file is part of the RESTBot Project.
+
+		Copyright (C) 2007-2008 PLEIADES CONSULTING, INC; (C) 2021 Gwyneth Llewelyn
+
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU Affero General Public License as
+		published by the Free Software Foundation, either version 3 of the
+		License, or (at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU Affero General Public License for more details.
+
+		You should have received a copy of the GNU Affero General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+		----------------------------------------------------------------------------
+
+		Author: Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
+--------------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+using System.Net;
+using System.Threading;
+
+namespace RESTbot
+{
+	/// <summary>All-around utility class</summary>
+	public static class Utilities
+	{
+		#region name2key/key2name
+		/// <summary>Caches for name2key and key2name requests</summary>
+		protected static Dictionary<UUID, AutoResetEvent> NameLookupEvents = new Dictionary<UUID, AutoResetEvent>();
+		protected static Dictionary<UUID, AutoResetEvent> KeyLookupEvents = new Dictionary<UUID, AutoResetEvent>();
+		protected static Dictionary<UUID, String> avatarNames = new Dictionary<UUID, String>();
+		protected static Dictionary<String, UUID> avatarKeys = new Dictionary<String, UUID>();
+
+		/// <summary>
+		/// key2name (given an avatar UUID, returns the avatar name, if it exists)
+		/// </summary>
+		/// <param name="b">RESTbot object</param>
+		/// <param name="key">UUID of avatar to check</param>
+		/// <returns>Name of the avatar if it exists; String.Empty if not</returns>
+		public static string getName(RestBot b, UUID id)
+		{
+			DebugUtilities.WriteInfo("getName(): Looking up name for " + id.ToString());
+			lock (NameLookupEvents)
+			{
+				NameLookupEvents.Add(id, new AutoResetEvent(false));
+			}
+
+			b.Client.Avatars.RequestAvatarName(id);
+
+			if (!NameLookupEvents[id].WaitOne(15000, true))
+			{
+				DebugUtilities.WriteWarning("getName(): timed out on avatar name lookup");
+			}
+			lock (NameLookupEvents)
+			{
+				NameLookupEvents.Remove(id);
+			}
+			// C# 8+ is stricter with null assignments.
+			string? response = null;	// technically this cannot ever be null, so it doesn't make sense...
+			if (avatarNames.ContainsKey(id))
+			{
+				response = avatarNames[id]; // .Name removed
+				lock (avatarNames)
+				{
+					avatarNames.Remove(id);
+				}
+			}
+			else
+			{
+				response = String.Empty;
+			}
+			return response;
+		} // end getName()
+
+		/// <summary>
+		/// Loop through all (pending) replies for UUID/Avatar names
+		/// and process them if they contain any key we're looking for.
+		/// </summary>
+		/// <param name="sender">parameter ignored</param>
+		/// <param name="e">List of UUID/Avatar names</param>
+		/// <returns>void</returns>
+		/// <remarks>obsolete syntax changed</remarks>
+		private void Avatars_OnAvatarNames(object sender, UUIDNameReplyEventArgs e)
+		{
+			DebugUtilities.WriteInfo("Avatars_OnAvatarNames(): Processing " + e.Names.Count.ToString() + " AvatarNames replies");
+			foreach (KeyValuePair<UUID, string> kvp in e.Names)
+			{
+				if (!avatarNames.ContainsKey(kvp.Key) || avatarNames[kvp.Key] == null)
+				{
+					DebugUtilities.WriteInfo("Avatars_OnAvatarNames(): Reply Name: " + kvp.Value + " Key : " + kvp.Key.ToString());
+					lock (avatarNames)
+					{
+						// avatarNames[kvp.Key] = new Avatar(); // why all this trouble?
+						// FIXME: Change this to .name when we move inside libsecondlife
+						// avatarNames[kvp.Key].Name = kvp.Value; // protected
+						avatarNames[kvp.Key] = kvp.Value;
+					}
+					if (NameLookupEvents.ContainsKey(kvp.Key))
+					{
+						NameLookupEvents[kvp.Key].Set();
+					}
+				}
+			}
+		} // end Avatars_OnAvatarNames()
+
+		/// <summary>
+		/// name2key (avatar name to UUID)
+		/// </summary>
+		/// <param name="b">RESTbot object</param>
+		/// <param name="name">Name of avatar to check</param>
+		/// <returns>UUID of corresponding avatar, if it exists</returns>
+		public static UUID getKey(RestBot b, String name)
+		{
+			DebugUtilities.WriteInfo("getKey(): Looking up key for " + name);
+			name = name.ToLower();
+			DebugUtilities.WriteDebug("Looking up: " + name);
+			DebugUtilities.WriteDebug("Key not in cache, requesting directory lookup");
+			lock (KeyLookupEvents)
+			{
+				KeyLookupEvents.Add(name, new AutoResetEvent(false));
+			}
+			DebugUtilities.WriteDebug("Lookup Event added, KeyLookupEvents now has a total of " + KeyLookupEvents.Count.ToString() + " entries");
+			DirFindQueryPacket find = new DirFindQueryPacket();
+			find.AgentData.AgentID = b.Client.Self.AgentID;	// was Network and not Self
+			find.AgentData.SessionID = b.Client.Self.SessionID;
+			find.QueryData.QueryFlags = 1;
+			//find.QueryData.QueryText = Helpers.StringToField(name);
+			find.QueryData.QueryText = Utils.StringToBytes(name);
+			find.QueryData.QueryID = new UUID("00000000000000000000000000000001");
+			find.QueryData.QueryStart = 0;
+
+			b.Client.Network.SendPacket((Packet) find);
+			DebugUtilities.WriteDebug("Packet sent - KLE has " + KeyLookupEvents.Count.ToString() + " entries.. now waiting");
+			KeyLookupEvents[name].WaitOne(15000,true);
+			DebugUtilities.WriteDebug("Waiting done!");
+			lock (KeyLookupEvents)
+			{
+				KeyLookupEvents.Remove(name);
+			}
+			DebugUtilities.WriteDebug("Done with KLE, now has " + KeyLookupEvents.Count.ToString() + " entries");
+			UUID response = new UUID();
+			if (avatarKeys.ContainsKey(name))
+			{
+				response = avatarKeys[name];
+				lock (avatarKeys)
+				{
+					avatarKeys.Remove(name);
+				}
+			}
+			return response;
+		} // end getKey()
+
+		/// <summary>
+		/// Overloaded getKey() function, using first name and last name as parameters
+		/// </summary>
+		/// <param name="b">RESTbot object</param>
+		/// <param name="avatarFirstName">First name of avatar to check</param>
+		/// <param name="avatarLastName">Last name of avatar to check</param>
+		/// <returns>UUID of corresponding avatar, if it exists</returns>
+		public static UUID getKey(RestBot b, String avatarFirstName, String avatarLastName)
+		{
+			String avatarFullName = avatarFirstName.ToString() + " " + avatarLastName.ToString();
+			return getKey(b, avatarFullName);	// it will be set to lowercase by getKey() (gwyneth 20220126).
+		}
+		#endregion name2key/key2name
+	} // end class
+} // end namespace
