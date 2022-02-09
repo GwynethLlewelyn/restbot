1459e194614dd4a1ccdf171fdcb175a797d43834
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2022-02-09 16:27:49 +0000

    Feat: internal usage of Prettier
    I'm using Prettier, but others may not be, so I'm excluding it from Git. The changed PHP file was prettified, just as a test, no content was changed.

--- restbot-tools/restbot-api/util.php
+++ restbot-tools/restbot-api/util.php
@@ -18,106 +18,107 @@
 		You should have received a copy of the GNU Affero General Public License
 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------*/
-require_once 'include/funktions.php';
-require_once 'include/config.php';
+require_once "include/funktions.php";
+require_once "include/config.php";
 
-require_once 'include/avatars.php';
-require_once 'include/util.php';
+require_once "include/avatars.php";
+require_once "include/util.php";
 
-global $ownername ; global $authuser;
+global $ownername;
+global $authuser;
 if (
-	( $_SERVER['HTTP_X_SECONDLIFE_OWNER_NAME'] == $ownername ) ||
-	( $_REQUEST['psk'] == $authuser )
-	) {
+	$_SERVER["HTTP_X_SECONDLIFE_OWNER_NAME"] == $ownername ||
+	$_REQUEST["psk"] == $authuser
+) {
 	$authorized = true;
 }
 
-$command = $_REQUEST['command'];
+$command = $_REQUEST["command"];
 
-if ( $command == "getkey" ) {
-	if ( !$authorized ) {
-		genPipeError('auth');
+if ($command == "getkey") {
+	if (!$authorized) {
+		genPipeError("auth");
 	}
-	if ( isset($_REQUEST['name']) ) {
-		if ( $_REQUEST['base64'] == 1 ) {
-			$name = base64_decode($_REQUEST['name']);
+	if (isset($_REQUEST["name"])) {
+		if ($_REQUEST["base64"] == 1) {
+			$name = base64_decode($_REQUEST["name"]);
 		} else {
-			$name = urldecode($_REQUEST['name']);
+			$name = urldecode($_REQUEST["name"]);
 		}
 	} else {
-		genPipeError('param');
+		genPipeError("param");
 	}
 	$resp = avatarKey($name);
-	if ( $resp == null ) {
+	if ($resp == null) {
 		genPipeError("lookup");
-	} else if ( $resp == "0" ) {
+	} elseif ($resp == "0") {
 		genPipeError("found");
 	} else {
 		genPipeResponse($resp);
 	}
-} else if ( $command == "getname" ) {
-	if ( !$authorized) {
-		genPipeError('auth');
+} elseif ($command == "getname") {
+	if (!$authorized) {
+		genPipeError("auth");
 	}
-	if ( isset($_REQUEST['key']) ) {
-		if ( $_REQUEST['base64'] == 1 ) {
-			$key = base64_decode($_REQUEST['key']);
+	if (isset($_REQUEST["key"])) {
+		if ($_REQUEST["base64"] == 1) {
+			$key = base64_decode($_REQUEST["key"]);
 		} else {
-			$key = urldecode($_REQUEST['key']);
+			$key = urldecode($_REQUEST["key"]);
 		}
 	} else {
-		genPipeError('param');
+		genPipeError("param");
 	}
 	$resp = avatarName($key);
-	if ( $resp == null ) {
+	if ($resp == null) {
 		genPipeError("lookup");
-	} else if ( $resp == "0" ) {
+	} elseif ($resp == "0") {
 		genPipeError("found");
 	} else {
 		genPipeResponse($resp);
 	}
-} else if ( $command == "gethandle" ) {
-	if ( !$authorized ) {
-		genPipeError('auth');
+} elseif ($command == "gethandle") {
+	if (!$authorized) {
+		genPipeError("auth");
 	}
-	if ( isset($_REQUEST['sim']) ) {
-		if ( $_REQUEST['base64'] == 1 ) {
-			$sim = base64_decode($_REQUEST['sim']);
+	if (isset($_REQUEST["sim"])) {
+		if ($_REQUEST["base64"] == 1) {
+			$sim = base64_decode($_REQUEST["sim"]);
 		} else {
-			$sim = urldecode($_REQUEST['sim']);
+			$sim = urldecode($_REQUEST["sim"]);
 		}
 	} else {
-		genPipeError('param');
+		genPipeError("param");
 	}
 	$resp = regionHandle($sim);
-	if ( $resp == null ) {
+	if ($resp == null) {
 		genPipeError("lookup");
-	} else if ( $resp == "0" ) {
+	} elseif ($resp == "0") {
 		genPipeError("found");
 	} else {
 		genPipeResponse($resp);
 	}
-} else if ( $command == "getmap" ) {
-	if ( !$authorized ) {
-		genPipeError('auth');
+} elseif ($command == "getmap") {
+	if (!$authorized) {
+		genPipeError("auth");
 	}
-	if ( isset($_REQUEST['sim']) ) {
-		if ( $_REQUEST['base64'] == 1 ) {
-			$sim = base64_decode($_REQUEST['sim']);
+	if (isset($_REQUEST["sim"])) {
+		if ($_REQUEST["base64"] == 1) {
+			$sim = base64_decode($_REQUEST["sim"]);
 		} else {
-			$sim = urldecode($_REQUEST['sim']);
+			$sim = urldecode($_REQUEST["sim"]);
 		}
 	} else {
-		genPipeError('param');
+		genPipeError("param");
 	}
 	$resp = regionMap($sim);
-	if ( $resp == null ) {
+	if ($resp == null) {
 		genPipeError("lookup");
-	} else if ( $resp == "00000000000000000000000000000000" ) {
+	} elseif ($resp == "00000000000000000000000000000000") {
 		genPipeResponse("3ab7e2fa-9572-ef36-1a30-d855dbea4f92");
 	} else {
 		genPipeResponse(friendlyUUID($resp));
 	}
 } else {
- genPipeError('param');
+	genPipeError("param");
 }
